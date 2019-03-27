897f1ab95c05f1735fb68865a2dcf28ef7868c5b
hibikiyoshiyasu <gameword.hibiki@gmail.com>
2019-03-28 01:10:12 +0900

    効用ファイル

---
+++ Guard&Shooting/Utility/Utiliy.h
@@ -0,0 +1,27 @@
+﻿#pragma once
+
+namespace Utility
+{
+	// 数値制限
+	template<typename T>bool Clamp(T& t, T min, T max)
+	{
+		// tに代入(tがminより小さければminを入れる、
+		//         tがmaxより大きければmaxを入れる、
+		//         それ以外ならtを入れる)
+		// t = (t < min) ? min : (t > max) ? max : t;
+
+		if (t < min)
+		{
+			t = min;
+			return true;
+		}
+
+		if (t > max)
+		{
+			t = max;
+			return true;
+		}
+
+		return false;
+	}
+}
